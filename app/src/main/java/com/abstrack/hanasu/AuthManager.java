31163b0471fa835b709f54a5b31d1137eb29f5fa
SleepL1 <sleepx100@gmail.com>
2022-05-09 19:28:37 -0600

    Null Object reference due to static authmanager

--- app/src/main/java/com/abstrack/hanasu/AuthManager.java
+++ app/src/main/java/com/abstrack/hanasu/AuthManager.java
@@ -5,11 +5,7 @@ import com.google.firebase.auth.FirebaseUser;
 
 public class AuthManager {
 
-    private static FirebaseAuth fireAuth;
-
-    public AuthManager(){
-        fireAuth = FirebaseAuth.getInstance();
-    }
+    private static FirebaseAuth fireAuth = FirebaseAuth.getInstance();
 
     public static boolean isUserLogged() {
         FirebaseUser currentUser = fireAuth.getCurrentUser();
