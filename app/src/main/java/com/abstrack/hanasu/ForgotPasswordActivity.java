9d175d029ec4fea6ee7ef9ea3ba762a06765126a
SleepL1 <sleepx100@gmail.com>
2022-05-10 23:14:53 -0600

    Added recovery email capability

--- app/src/main/java/com/abstrack/hanasu/ForgotPasswordActivity.java
+++ app/src/main/java/com/abstrack/hanasu/ForgotPasswordActivity.java
@@ -1,17 +1,41 @@
 package com.abstrack.hanasu;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
 import android.view.View;
 
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.Task;
+import com.google.android.material.textfield.TextInputLayout;
+
 public class ForgotPasswordActivity extends BaseAppActivity {
+
+    private TextInputLayout emailTextInput;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_forgot_password);
+
+        emailTextInput = findViewById(R.id.textInputLayoutEmail);
     }
+
+    public void sendForgotPasswordCode(View view){
+        if(!AuthManager.validateEmailText(emailTextInput))
+            return;
+
+        String emailText = emailTextInput.getEditText().getText().toString();
+
+        AuthManager.getFireAuth().sendPasswordResetEmail(emailText)
+                .addOnCompleteListener(new OnCompleteListener<Void>() {
+                    @Override
+                    public void onComplete(@NonNull Task<Void> task) {
+                    }
+                });
+    }
+
     public void changeToLastActivity(View view) {
         finish();
     }
