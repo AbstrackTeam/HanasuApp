393d1eefde442675e3dfda38453011a72167518f
Dani <79950554+Kaserola4@users.noreply.github.com>
2022-05-19 21:56:03 -0500

    Now the open more options has animations

--- app/src/main/java/com/abstrack/hanasu/activity/LandingActivity.java
+++ app/src/main/java/com/abstrack/hanasu/activity/LandingActivity.java
@@ -1,11 +1,15 @@
 package com.abstrack.hanasu.activity;
 
+import androidx.appcompat.widget.AppCompatImageView;
 import androidx.cardview.widget.CardView;
 import androidx.recyclerview.widget.LinearLayoutManager;
 import androidx.recyclerview.widget.RecyclerView;
 
+import android.animation.Animator;
 import android.os.Bundle;
 import android.view.View;
+import android.view.animation.Animation;
+import android.widget.ImageView;
 import android.widget.Toast;
 
 import com.abstrack.hanasu.BaseAppActivity;
@@ -21,6 +25,10 @@ import java.util.List;
 public class LandingActivity extends BaseAppActivity {
 
     private RecyclerView storiesBar, chatsListView;
+    private CardView showMoreButton, addChatButton, addGroupsButton, searchButton;
+    private ImageView showMoreButtonIcon, addChatButtonIcon, addGroupsButtonIcon, searchButtonIcon;
+    private boolean showingMoreOptions;
+    private float showMoreButtonY, addChatButtonY, addGroupsButtonY, searchButtonY;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -29,9 +37,26 @@ public class LandingActivity extends BaseAppActivity {
         init();
     }
 
-    private void init(){
+    private void init() {
+        // Recycler views
         storiesBar = findViewById(R.id.storiesBar);
         chatsListView = findViewById(R.id.chatsListView);
+        // More options views
+        showMoreButton = findViewById(R.id.showMoreOptions);
+        addChatButton = findViewById(R.id.addChat);
+        addGroupsButton = findViewById(R.id.addGroup);
+        searchButton = findViewById(R.id.search);
+
+        showingMoreOptions = false;
+
+        showMoreButtonIcon = findViewById(R.id.moreOptionsIcon);
+        addChatButtonIcon = findViewById(R.id.addChatIcon);
+        addGroupsButtonIcon = findViewById(R.id.addGroupIcon);
+        searchButtonIcon = findViewById(R.id.searchIcon);
+
+        addChatButton.setVisibility(View.GONE);
+        addGroupsButton.setVisibility(View.GONE);
+        searchButton.setVisibility(View.GONE);
 
         List<Story> stories = new ArrayList<>();
         List<Chat> chats = new ArrayList<>();
@@ -49,29 +74,122 @@ public class LandingActivity extends BaseAppActivity {
         stories.add(new Story(false));
 
         // Chats
-        chats.add(new Chat(false, "Bochin",0, 7, "Hola amigo emoticón \uD83D\uDE00 desgraciado", "2:34 AM"));
-        chats.add(new Chat(false, "Soy una persona insaciable",0, 4, "Hola amigo emoticón \uD83D\uDE00 desgraciado", "2:34 AM"));
+        chats.add(new Chat(false, "Bochin", 0, 7, "Hola amigo emoticón \uD83D\uDE00 desgraciado", "2:34 AM"));
+        chats.add(new Chat(false, "Soy una persona insaciable", 0, 4, "Hola amigo emoticón \uD83D\uDE00 desgraciado", "2:34 AM"));
 
-        StoriesAdapter storiesAdapter = new StoriesAdapter(stories, storiesBar,this);
+        // Set the stories adapter
+        StoriesAdapter storiesAdapter = new StoriesAdapter(stories, storiesBar, this);
         storiesBar.setAdapter(storiesAdapter);
         storiesBar.setLayoutManager(new LinearLayoutManager(this, RecyclerView.HORIZONTAL, false));
         storiesBar.setItemAnimator(null);
 
+        // Set the chats adapter
         ChatsAdapter chatsAdapter = new ChatsAdapter(chats, this);
         chatsListView.setAdapter((chatsAdapter));
         chatsListView.setLayoutManager(new LinearLayoutManager(this, RecyclerView.VERTICAL, false));
 
-        CardView addChatButton = findViewById(R.id.moreOptions);
+        buildOptionsListeners();
+    }
+
+    public void buildOptionsListeners() {
+        showMoreButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                animateOptions();
+            }
+        });
+
+
         addChatButton.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
-                addChat();
+                // TODO
             }
         });
     }
 
-    public void addChat(){
-        // TODO
-        Toast.makeText(this, "add chat button clicked", Toast.LENGTH_SHORT).show();
+    public void animateOptions() {
+        if (!showingMoreOptions) {
+            showMoreButtonIcon.setImageResource(R.drawable.ic_down_arrow);
+
+            showMoreButton.setClickable(false);
+            addChatButton.setClickable(false);
+            addGroupsButton.setClickable(false);
+            searchButton.setClickable(false);
+
+            showMoreButton.postDelayed(new Runnable() {
+                @Override
+                public void run() {
+                    showMoreButton.setClickable(true);
+                    addChatButton.setClickable(true);
+                    addGroupsButton.setClickable(true);
+                    searchButton.setClickable(true);
+                    showingMoreOptions = true;
+                }
+            }, 900);
+
+            addChatButton.setVisibility(View.VISIBLE);
+            addGroupsButton.setVisibility(View.VISIBLE);
+            searchButton.setVisibility(View.VISIBLE);
+
+            addChatButton.setAlpha(0.5f);
+            addChatButton.setTranslationY(0);
+            addChatButton.setTranslationY(showMoreButton.getY() - addChatButton.getY());
+            addChatButton.animate()
+                    .alpha(1.0f).
+                    translationY(0)
+                    .setDuration(300);
+
+            addGroupsButton.setAlpha(0.5f);
+            addGroupsButton.setTranslationY(0);
+            addGroupsButton.setTranslationY(showMoreButton.getY() - addGroupsButton.getY());
+            addGroupsButton.animate()
+                    .alpha(1.0f)
+                    .translationY(0)
+                    .setDuration(300);
+
+            searchButton.setAlpha(0.5f);
+            searchButton.setTranslationY(0);
+            searchButton.setTranslationY(showMoreButton.getY() - searchButton.getY());
+            searchButton.animate()
+                    .alpha(1.0f)
+                    .translationY(0)
+                    .setDuration(300);
+            return;
+        }
+        showMoreButton.setClickable(false);
+        addChatButton.setClickable(false);
+        addGroupsButton.setClickable(false);
+        searchButton.setClickable(false);
+        showMoreButton.postDelayed(new Runnable() {
+            @Override
+            public void run() {
+                addChatButton.setVisibility(View.GONE);
+                addGroupsButton.setVisibility(View.GONE);
+                searchButton.setVisibility(View.GONE);
+                showMoreButton.setClickable(true);
+                addChatButton.setClickable(true);
+                addGroupsButton.setClickable(true);
+                searchButton.setClickable(true);
+                showingMoreOptions = false;
+            }
+        }, 900);
+
+        showMoreButtonIcon.setImageResource(R.drawable.ic_plus);
+        addChatButton.animate()
+                .translationY(showMoreButton.getY() - addChatButton.getY())
+                .setDuration(300);
+        addGroupsButton.animate().
+                translationY(showMoreButton.getY() - addGroupsButton.getY())
+                .setDuration(300);
+        searchButton.animate().
+                alpha(0.0f).
+                setDuration(0);
+        searchButton.animate()
+                .alpha(0.0f).
+                translationY(showMoreButton.getY() - searchButton.getY())
+                .setDuration(300);
     }
+
 }
+
