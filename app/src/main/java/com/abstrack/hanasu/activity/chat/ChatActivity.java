a743c8d43dfd74f6d5cb402cdea9c3e7474d554e
SleepL1 <sleepx100@gmail.com>
2022-05-30 12:34:32 -0600

    ActivityChat created

---
+++ app/src/main/java/com/abstrack/hanasu/activity/chat/ChatActivity.java
@@ -0,0 +1,15 @@
+package com.abstrack.hanasu.activity.chat;
+
+import android.os.Bundle;
+
+import com.abstrack.hanasu.BaseAppActivity;
+import com.abstrack.hanasu.R;
+
+public class ChatActivity extends BaseAppActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_loading);
+    }
+}
